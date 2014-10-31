a4b9aaacb83d87f948259e32087b47956bfa8319
Uffe <Uffe@10.50.131.14>
2014-10-31 09:57:40 +0100

    Tilføjelse af klassen "Order" øhhh

---
+++ Group_07_2sem/src/domain/Order.java
@@ -0,0 +1,99 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package domain;
+
+import java.sql.Date;
+
+
+
+/**
+ *
+ * @author Uffe
+ */
+public class Order {
+    private int orderID;
+    private float price;
+    private Date startDate;
+    private Date endDate;
+    private boolean comfirmed;
+    private int customerID; //skal laves om til Customer Object senere
+    private int salesID; // skal laves om til Salesman Object senere
+   
+    
+    public Order(int orderID, float price, long startDate, long endDate,
+                 boolean confirmed) { // Der mangler stadig Customer og Sales i constructor
+        this.orderID = orderID;
+        this.price = price;
+        this.comfirmed = confirmed;
+        this.startDate = new Date(startDate);
+        this.endDate = new Date(endDate);
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" + "orderID=" + orderID + ", price=" + price + ", startDate=" + startDate + ", endDate=" + endDate + ", comfirmed=" + comfirmed + ", customerID=" + customerID + ", salesID=" + salesID + '}';
+    }
+    
+    
+    public int getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(int orderID) {
+        this.orderID = orderID;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
+    }
+
+    public boolean isComfirmed() {
+        return comfirmed;
+    }
+
+    public void setComfirmed(boolean comfirmed) {
+        this.comfirmed = comfirmed;
+    }
+
+    public int getCustomerID() {
+        return customerID;
+    }
+
+    public void setCustomerID(int customerID) {
+        this.customerID = customerID;
+    }
+
+    public int getSalesID() {
+        return salesID;
+    }
+
+    public void setSalesID(int salesID) {
+        this.salesID = salesID;
+    }
+    
+    
+     
+}
