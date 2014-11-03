b4ca610e8ccf955c9007ebf5ca4007ebc26b6eed
Flazhed <soren1337@gmail.com>
2014-11-03 14:53:35 +0100

    Orderdetail storageID added

--- Group_07_2sem/src/domain/OrderDetail.java
+++ Group_07_2sem/src/domain/OrderDetail.java
@@ -13,17 +13,19 @@ public class OrderDetail {
     //Tilføj storage ID og fix toString
     private int orderID;
     private int resourceID;
+    private int storageID;
     private int qty;
 
-    public OrderDetail(int orderID, int resourceID, int qty) {
+    public OrderDetail(int orderID, int resourceID, int storageID, int qty) {
         this.orderID = orderID;
         this.resourceID = resourceID;
+        this.storageID = storageID;
         this.qty = qty;
     }
 
     @Override
     public String toString() {
-        return "OrderID: " + orderID + ", resourceID: " + resourceID + ", qty: " + qty + '}';
+        return "OrderID: " + orderID + ", resourceID: " + resourceID + ", StorageID: " + storageID + ", qty: " + qty + '}';
     }
     
     public int getOrderID() {
