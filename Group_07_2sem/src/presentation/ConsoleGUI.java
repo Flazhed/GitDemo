856ea0d2c498b9f25aa75fe1fa35d6ee920706d0
Schultzz <MS@10.50.130.118>
2014-11-04 08:45:10 +0100

    Fixet dato format

--- Group_07_2sem/src/presentation/ConsoleGUI.java
+++ Group_07_2sem/src/presentation/ConsoleGUI.java
@@ -6,23 +6,34 @@
 package presentation;
 
 import domain.*;
-import java.util.ArrayList;
+import java.sql.Date;
+import java.text.ParseException;
 
 /**
  *
  * @author Uffe
  */
 public class ConsoleGUI {
-    
-    public static void main(String[] args) {
-        ArrayList<OrderDetail> odl = new ArrayList<OrderDetail>();
+
+    public static void main(String[] args) throws ParseException {
+//        ArrayList<OrderDetail> odl = new ArrayList();
         Controller con = Controller.getInstance();
-        System.out.println(con.getOrder(1111).toString());
-        Order o = con.getOrder(1111);
-        odl.add(con.createNewOrderDetail(1111, 3, 1, 5));
-        con.insertOrderDetails(odl);
-        System.out.println(con.getOrder(1111).toString());
-      
+
+        // date(dato - 1900, month from 00(jan) to 11(dec), days 01-31)
+        Date startDate = new Date(2014 - 1900, 11, 24);
+        Date endDate = new Date(2015 - 1900, 00, 30);
+
+        con.insertOrder(con.createNewOrder(4444, 2, 22, false, startDate, endDate, 123.90f));
+        System.out.println(startDate);
+
+        System.out.println(con.getOrder(4444));
+        
+        
+//        Order o = con.getOrder(1111);
+//        odl.add(con.createNewOrderDetail(1111, 3, 1, 5));
+//        con.insertOrderDetails(odl);
+//        System.out.println(con.getOrder(1111));
+
     }
-    
+
 }
